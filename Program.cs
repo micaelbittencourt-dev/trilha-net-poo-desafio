c2050ff24e17773c39f41049664f254438521847
Micael Bittencourt <micaelbittencourt.dev@gmail.com>
2024-05-01 21:30:02 -0300

    Adicionada a herança e polimorfismo nas classes Iphone e Nokia, tambem criado o teste na Program.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "222222222", "3333333333", 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("--------------------------------------------------------");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("123456", "222222222", "3333333333", 4);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
